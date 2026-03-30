591dc1460f355bf007778ca48cf8dc5aed649630
Bruno Carvalho <brunocarvalhosoares.dev@gmail.com>
2026-03-29 23:48:44 -0300

    Add: new videos file

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,32 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import { FaLinkedin, FaGithub , FaWhatsapp, FaEnvelope} from 'react-icons/fa6';
+import videoContato from './assets/videos/video_contato_boneco.mp4';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+            
+      <section id="top">
+          <h1>Bem-vindo ao meu blog pessoal!</h1>
+      </section>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
-        <div>
+        
           <h1>Olá, tudo bem? Meu nome é Bruno Carvalho.</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
       </section>
-
       <div className="ticks"></div>
-
       <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
 
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
+        <div id="center">
           <h2>Contato</h2>
           <p>Me mande uma mensagem, será um prazer te conhecer!</p>
           <ul>
@@ -84,7 +52,7 @@ function App() {
                   <FaLinkedin className="button-icon" />
                 </svg>
                 LinkedIn
-              </a>
+              </a> 
             </li>
             <li>
               <a href="https://wa.me/+5581973065923?" target="_blank">
@@ -100,7 +68,7 @@ function App() {
             </li>
             <li>
               <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
+                <svg 
                   className="button-icon"
                   role="presentation"
                   aria-hidden="true"
@@ -111,6 +79,9 @@ function App() {
               </a>
             </li>
           </ul>
+        </div>
+        <div id="center">
+          <video src={videoContato} autoPlay loop muted playsInline width={125}/>
         </div>
       </section>
 
