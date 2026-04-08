983dd52bbee0b0fc8cba8eaaf7ca5f1a5b8ed729
Bruno Carvalho <brunocarvalhosoares.dev@gmail.com>
2026-04-07 23:44:35 -0300

    Add: initial contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,5 @@
 import { FaLinkedin, FaGithub } from 'react-icons/fa6';
+import Button from './ui/Button';
 
 function Contact() {
     return (
@@ -10,9 +11,9 @@ function Contact() {
                     <h2 className='text-lg md:text-xl  text-gray-300 m-0'>Siga-me e envie uma mensagem, será um prazer te conhecer!</h2>
                 </div>
 
-                <div className=' text-white flex justify-center py-5 gap-10' id="contact">
+                <div className=' text-white justify-center py-5 gap-10 grid grid-cols-1 md:grid-cols-2 ' id="contact">
                     <div className='flex items-center'>   
-                        <ul className='text-white flex gap-5'>
+                        <ul className='text-white flex flex-col gap-5'>
                             <li>
                                 <a href="https://github.com/BrunoCarvSo" target="_blank" rel='noopener noreferrer' className='flex items-center gap-2 hover:text-purple-400'>
                                 <FaGithub/>
@@ -31,10 +32,38 @@ function Contact() {
                             </li>
 
                         </ul>
+
+                        <div>
+                            <video src={"\\videos\\video_contato.mp4"} autoPlay loop muted playsInline width={200}/>
+                        </div>
                     </div>
 
                     <div>
-                        <video src={"\\videos\\video_contato.mp4"} autoPlay loop muted playsInline width={100}/>
+                        <form className='flex flex-col gap-5 bg-gray-900 p-8 rounded-2xl border border-gray-600'>
+
+                            {/* Campo Nome */}
+                            <div className="flex flex-col gap-2">
+                                <label className="text-sm font-semibold text-gray-300">Seu Nome</label>
+                                <input type="text" name="name" required className="p-3 rounded-lg bg-gray-800 text-white" />
+                            </div>
+
+                            <div className='flex flex-col gap-2'>
+                                {/* Campo Email */}
+                                <label className="text-sm font-semibold text-gray-300">Seu Email</label>
+                                <input type="email" name="email" required className="p-3 rounded-lg bg-gray-800 text-white" />
+                            </div>
+
+                            <div className='flex flex-col gap-2'>
+                                {/* Campo Mensagem */}
+                                <label className='text-sm font-semibold text-gray-300'>Sua mensagem</label>
+                                <input type='text' name='message' required className='p-3 rounded-lg bg-gray-800 text-white' />
+                            </div>
+
+                            <button type="submit" className='bg-purple-500 hover:bg-purple-600 text-white font-semibold py-3 rounded-lg transition-colors duration-300'>
+                                Enviar Mensagem
+                            </button>
+
+                        </form>
                     </div>
                 </div>
 
