400ef0f30f145fd020422ad1e7762d65e2867a5a
Bruno Carvalho <brunocarvalhosoares.dev@gmail.com>
2026-04-13 22:06:43 -0300

    ADD: hamburguer menu for small screens

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,18 +1,30 @@
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import Button from './ui/Button';   
+import { useState } from 'react';
 
 function Navigation(){
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <>
-            <nav className="fixed top-0 left-0 w-full bg-linear-to-r from-blue-500 to-purple-500 flex justify-end border-b border-white p-4 z-50">
-                <Button><Link to="/">Início</Link></Button>
-                <Button><Link to="/aboutMe">Sobre Mim</Link></Button>
-                <Button><HashLink to="/#technologies">Tecnologias</HashLink></Button>
-                <Button><HashLink to="/#contact">Contato</HashLink></Button>
-                <Button><Link to="/blog">Blog</Link></Button>
-            </nav>
-        </>
+
+        <nav className="fixed top-0 left-0 w-full bg-linear-to-r from-blue-500 to-purple-500 flex justify-end border-b border-white p-4 z-50">
+            <button onClick={() => setIsOpen(!isOpen)} className="md:hidden text-white text-2xl font-bold cursor-pointer">
+                    {isOpen ? '✕' : '☰'}
+            </button>
+
+                <div className={`
+                    ${isOpen ? 'flex' : 'hidden'} 
+                    md:flex absolute md:relative top-full right-0 w-full md:w-auto flex-col md:flex-row 
+                    bg-purple-600 md:bg-transparent p-4 md:p-0 gap-4 border-b border-white md:border-none
+                `}>
+                    <Button><Link onClick={() => setIsOpen(false)} to="/">Início</Link></Button>
+                    <Button><Link onClick={() => setIsOpen(false)} to="/aboutMe">Sobre Mim</Link></Button>
+                    <Button><HashLink onClick={() => setIsOpen(false)} to="/#technologies">Tecnologias</HashLink></Button>
+                    <Button><HashLink onClick={() => setIsOpen(false)} to="/#contact">Contato</HashLink></Button>
+                    <Button><Link onClick={() => setIsOpen(false)} to="/blog">Blog</Link></Button>
+                </div>
+        </nav>
+
     )
 }
 export default Navigation
